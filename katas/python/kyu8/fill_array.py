aef8699640b7ea9577a6cb5d9b1c1def1fb45364
ariela <ariela.haley65@gmail.com>
2020-09-28 20:02:36 +0100

    In progress kata

--- katas/python/kyu8/fill_array.py
+++ katas/python/kyu8/fill_array.py
@@ -2,9 +2,7 @@
 Write a function that produces an array with the numbers 0 to N-1 in it.
 """
 def fill_array(n = ""): 
-    # [ the numbers 0 to N-1 ]
     lists = []
-    # create a loop that counts n times and store it in a list 
     if n:
         for num in range(n - 1 + 1): 
             lists.append(num)
