9d64fbe03eec0fa962cacf48b7373c60623247b0
Ari <36749814+ari-hacks@users.noreply.github.com>
2021-01-14 23:07:36 -0800

    python kata-kyu7

--- katas/python/kyu7/isogram.py
+++ katas/python/kyu7/isogram.py
@@ -1,6 +1,2 @@
 def is_isogram(string):
-    string_to_list = list(string.lower())
-  
-    return False if len(string_to_list) != len(set(string_to_list)) else True
-
-# print(is_isogram("mose"))
+    return False if len(string.lower()) != len(set(string.lower())) else True
